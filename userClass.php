4d668e07e950e102b3fab395823d57ab37d5b347
Toms Karlis Ginters <144908700+Ginca1@users.noreply.github.com>
2023-10-02 11:12:45 +0300

    Add files via upload

--- userClass.php
+++ userClass.php
@@ -4,5 +4,12 @@ class User{
     private $lietotajvards;
     private $epasts;
     private $parole;
+
+    public function __construct($id, $lietotajvards, $epasts, $parole) {
+        $this->id = $id;
+        $this->lietotajvards = $lietotajvards;
+        $this->epasts = $epasts;
+        $this->parole = $parole;
+    }
 }
 ?>
