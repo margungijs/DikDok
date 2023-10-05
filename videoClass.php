8a114cff228828fd3bf5523d1ab2973c1a3756a3
Alekss Velvelis <alekssvelvelis@Aleksss-MacBook-Air.local>
2023-10-05 13:43:08 +0300

    Added videoClass.php, completed DIK-1

--- videoClass.php
+++ videoClass.php
@@ -4,13 +4,6 @@ class Video{
     private $url;
     private $autora_id;
     private $nosaukums;
-
-    public function __construct($id, $url, $autora_id, $nosaukums) {
-        $this->id = $id;
-        $this->url = $url;
-        $this->autora_id = $autora_id;
-        $this->nosaukums = $nosaukums;
-    }
 }
 
 
